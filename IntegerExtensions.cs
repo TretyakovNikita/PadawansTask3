83a118939404db4c4b88e0d47c092f38a494aa3c
Nikita Tret <accauntnikity@gmail.com>
2019-02-11 03:30:33 +0300

    Update IntegerExtensions.cs

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,7 +6,18 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException();
+            }
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            return GcdInner(a, b);
+        }
+        private static int GcdInner(int a, int b)
+        {
             return b == 0 ? a : Gcd(b, a % b);
         }
     }
 }
+
